0a1ad35f365d636336e33af3c18372440bbd1a7c
AdonNeet <baradontap@gmail.com>
2025-06-10 23:13:02 +0700

    chores: other majormajormajormajormajormajor

--- src/lib/services/user.js
+++ src/lib/services/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const supabase = require("../../config/supabaseClient");
 
 const createUser = async (user) => {
@@ -20,20 +21,48 @@ const updateUser = async (user) => {
       email: user.email,
       password: user.password,
     })
-    .eq("user_id", user.user_id)
+    .eq("id_user", user.id_user)
     .select()
     .single();
 };
 
 const getUserByCredential = async (nama, password) => {
-  const key = nama.includes("@") ? "email" : "nama";
-  return await supabase
-    .from("users")
-    .select("id_user, nama, email")
-    .eq(key, nama)
-    .eq("password", password)
-    .maybeSingle();
+  try {
+    const key = nama.includes("@") ? "email" : "nama";
+
+    const { data: user, error } = await supabase
+      .from("users")
+      .select("id_user, nama, password, email")
+      .eq(key, nama)
+      .maybeSingle();
+
+    if (error) {
+      return { data: null, error };
+    }
+
+    if (!user || password !== user.password) {
+      return {
+        data: null,
+        error: new Error("Invalid credentials"),
+      };
+    }
+
+    return {
+      data: {
+        id_user: user.id_user,
+        nama: user.nama,
+        email: user.email,
+      },
+      error: null,
+    };
+  } catch (err) {
+    return {
+      data: null,
+      error: new Error("Internal server error"),
+    };
+  }
 };
+
 
 const getUserById = async (id_user) => {
   return await supabase
